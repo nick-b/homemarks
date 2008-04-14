7d33fe887ca36b917c55ef90d4bbae2010ee98f3
Ken Collins <ken@metaskills.net>
2008-04-14 00:03:06 -0400

    Adding tests for JsMin initialization

--- config/initializers/homemarks.rb
+++ config/initializers/homemarks.rb
@@ -1,5 +1,8 @@
 
+# Simple configuration class.
+# -----------------------------------------------------------------------
 class HmConfig
+  JSMIN_TARGETS = ['prototype','effects','bookmarklet']
   @@property = {
     :app => {
       :salt                 => 'ase282193kkwdsfdsfjASDfe829234348',
@@ -24,18 +27,13 @@ class HmConfig
 end
 
 
-# Setting up the UUID state file and logger:
+# Setting up the UUID state file and logger.
 # -----------------------------------------------------------------------
 UUID.state_file = File.join(RAILS_ROOT, "tmp", "uuid.state")
 UUID.setup unless File.exists?(UUID.state_file)
 UUID.config :state_file => UUID.state_file, :logger => RAILS_DEFAULT_LOGGER
 
-
 # Setting up streamlined js files for use with the bookmarklet code.
 # -----------------------------------------------------------------------
-unless ENV['SKIPTHIS'] == 'true'
-  JsMin.optimize('prototype.js')
-  JsMin.optimize('effects.js')
-  JsMin.optimize('bookmarklet.js')  
-end
+HmConfig::JSMIN_TARGETS.each { |target| JsMin.optimize("#{target}.js") }
 

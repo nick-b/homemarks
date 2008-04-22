ca7c99173006b6072aa043e0377823cf04891ff3
Ken Collins <ken@metaskills.net>
2008-04-22 19:12:31 -0400

    Adding content to site index. Adding utility_styles.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,6 +1,6 @@
 module SiteHelper
   
-  def top_nav_for(name,options={})
+  def site_nav_for(name,options={})
     options.assert_valid_keys :url
     img = image_tag "/stylesheets/images/site/nav_#{options[:img] || name.underscore}.png", :alt => name, :border => 'none'
     link_to img+name, options[:url], :onclick => 'this.blur();'

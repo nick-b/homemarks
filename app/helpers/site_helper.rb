e355194ceed72a5fbecb193517e73d0531e3bd11
Ken Collins <ken@metaskills.net>
2008-04-29 21:48:08 -0400

    Logged in as bob support requets function tests. Fix site_nav_for helper when logged in.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,7 +1,7 @@
 module SiteHelper
   
   def site_nav_for(name,options={})
-    options.assert_valid_keys :url
+    options.assert_valid_keys :url, :img
     img = image_tag "/stylesheets/images/site/nav_#{options[:img] || name.underscore}.png", :alt => name, :border => 'none'
     link_to img+name, options[:url], :onclick => 'this.blur();'
   end
--- test/functional/support_requests_controller_test.rb
+++ test/functional/support_requests_controller_test.rb
@@ -21,19 +21,36 @@ class SupportRequestsControllerTest < ActionController::TestCase
       assert_element_visible '#ajaxforms_wrapper'
     end
     
+    should 'populate support form with user email when logged in' do
+      login_as(:bob)
+      get :new, {:show_form => 'true'}
+      assert_select 'input#support_request_email[value=?]', users(:bob).email
+    end
+    
   end
   
   context 'While testing the create action' do
-
+    
     should 'create a new support request' do
-      xhr :post, :create, {:support_request => {:email => 'user@test.com', :problem => 'Account Issues', :details => 'Test'}}
+      xhr_create_support_request
       assert_instance_of SupportRequest, assigns(:support_request)
       assert_response :success, "object was not saved, errors are #{inspect_errors(assigns(:support_request))}"
+    end
+    
+    should 'create a new support request bound to a user when logged in' do
+      login_as(:bob)
+      xhr_create_support_request
+      assert_equal users(:bob), assigns(:support_request).user
     end
 
   end
   
   
+  protected
+  
+  def xhr_create_support_request
+    xhr :post, :create, {:support_request => {:email => 'user@test.com', :problem => 'Account Issues', :details => 'Test'}}
+  end
   
   
 end

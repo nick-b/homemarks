5afe4c4a38daa080396c13c19122fc66b91f5501
Ken Collins <ken@metaskills.net>
2008-05-27 01:03:08 -0400

    Sweet flash messages for app in JS class now. It can now cope with many messages in series too.

--- public/javascripts/homemarks/app.js
+++ public/javascripts/homemarks/app.js
@@ -2,11 +2,33 @@
 var HomeMarksApp = Class.create(HomeMarksBase,{
   
   initialize: function() {
+    this.loading = $('loading');
     this.modal = new HomeMarksModal();
     this.initEvents();
   },
   
   
+  
+  flash: function(mood,message) {
+    this.hud = $('hud');
+    this.messageArea = $('message_wrapper');
+    this.resetFlash();
+    this.resetFlashEffect();
+    this.hud.addClassName(mood);
+    this.messageArea.update(SPAN({id:'message'},message));
+    this.flashEffect = setTimeout(function(){
+      this.resetFlash();
+      $('message').fade();
+    }.bind(this),5000);
+  },
+  
+  resetFlash: function() {
+    this.flashMoods.each(function(m){ this.hud.removeClassName(m); }.bind(this));
+  },
+  
+  resetFlashEffect: function() {
+    if (this.flashEffect) { clearTimeout(this.flashEffect); };
+  },
   
   initEvents: function() {
     

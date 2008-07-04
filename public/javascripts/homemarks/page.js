938eeae25d76a1cb4aaea3e6f433a1ce570bc962
Ken Collins <ken@metaskills.net>
2008-07-03 22:56:04 -0400

    Incomplete shift into parent objects creating child sortable.

---
+++ public/javascripts/homemarks/page.js
@@ -0,0 +1,44 @@
+
+var Page = Class.create(HomeMarksApp,{
+  
+  initialize: function($super) { 
+    $super();
+    this.sortable = this.pageSortable;
+    this._buildColumnsSortable();
+  },
+  
+  columns: function() {
+    return Columns;
+  },
+  
+  columnSortParams: function() {
+    return SortableUtils.getSortParams(this.sortable);
+  },
+  
+  completeColumnSort: function() {
+    this.flash('good','Columns sorted.');
+    SortableUtils.resetSortableLastValue(this.sortable);
+  },
+  
+  _buildColumnsSortable: function() {
+    this.sortable.action = '/columns/sort';
+    this.sortable.parameters = this.columnSortParams;
+    this.sortable.method = 'put';
+    Sortable.create(this.sortable, {
+      handle:       'ctl_handle', 
+      tag:          'div', 
+      accept:       'dragable_columns', 
+      containment:  this.sortable.id,
+      constraint:   false, 
+      dropOnEmpty:  true, 
+      onUpdate: this.startAjaxRequest.bindAsEventListener(this,{onComplete:this.completeColumnSort}), 
+    });
+  }
+  
+});
+
+
+document.observe('dom:loaded', function(){
+  Page.obj = new Page();
+});
+

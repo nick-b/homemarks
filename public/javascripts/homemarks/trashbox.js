0264d83715e3d95ff010f8c1460dd2584faf8f50
Ken Collins <ken@metaskills.net>
2008-08-02 22:15:52 -0400

    Nice... trashbox is now working.

--- public/javascripts/homemarks/trashbox.js
+++ public/javascripts/homemarks/trashbox.js
@@ -6,29 +6,29 @@ var TrashboxClass = Class.create(HomeMarksApp,ActionBoxMixins,BookmarkSortableMi
     this.superActionAreaMixins();
     this.class = 'Trashbox';
     this.box = $('trashbox');
-    this.id = parseInt(this.sortableList().id.sub('trashbox_list_',''));
+    this.id = parseInt(this.trashboxList.id.sub('trashbox_list_',''));
     this._initTrashboxEvents();
   },
   
   open: function() {
     this.setField(this.legendTrashbox);
-    this.loadTrashbox();
-    this.showFieldsetProgress();
-    this.trashboxList.blindUp({duration: 0.35});
+    this.load();
+    this.hideProgressAndShowList(this.trashboxList);
   },
   
-  loadTrashbox: function() {
+  load: function() {
     if (this.trashboxList.loaded) { return true };
     var request = new Ajax.Request('/trashbox/bookmarks',{asynchronous:false,method:'get'});
-    var bookmarks = request.transport.responseText.evalJSON();
+    var bookmarkData = request.transport.responseText.evalJSON();
+    bookmarkData.each(function(bm){
+      new BookmarkBuilder(this,bm.bookmark);
+    }.bind(this));
     this.trashboxList.loaded = true;
   },
   
-  
-  
   _initTrashboxEvents: function() {
-    // this._buildBookmarksSortables();
-    // this.legendTrashbox.observe('click',this.showTrashbox.bindAsEventListener(this));
+    this._buildBookmarksSortables();
+    this.legendTrashbox.observe('click',this.open.bindAsEventListener(this));
   }
   
 });

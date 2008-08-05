1d1746c061f90e56ed470c250fca3f8d4032b109
Ken Collins <ken@metaskills.net>
2008-08-05 00:06:34 -0400

    Remove OLD rails sortable overrides.

--- config/initializers/homemarks.rb
+++ config/initializers/homemarks.rb
@@ -1,6 +1,7 @@
 
 # Simple configuration class.
 # -----------------------------------------------------------------------
+
 class HmConfig
   @@property = {
     :app => {
@@ -24,6 +25,7 @@ end
 
 # Setting up the UUID state file and logger.
 # -----------------------------------------------------------------------
+
 UUID.state_file = File.join(RAILS_ROOT, "tmp", "uuid.state")
 UUID.setup unless File.exists?(UUID.state_file)
 UUID.config :state_file => UUID.state_file, :logger => RAILS_DEFAULT_LOGGER
